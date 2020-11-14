cd4643bf7d7881c6bd62cffa931eeb98d8df8087
Apple <morcodou@hotmail.com>
2020-11-13 23:44:00 -0500

    adding imahe resource

---
+++ resources/image.py
@@ -0,0 +1,28 @@
+from flask_restful import Resource
+from flask_uploads import UploadNotAllowed
+from flask import request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
+from libs import image_helper
+from libs.strings import gettext
+from schemas.image import ImageSchema
+
+image_schema = ImageSchema()
+
+
+class ImageUpload(Resource):
+    @jwt_required
+    def post(self):
+        data = image_schema.load(request.files)
+        user_id = get_jwt_identity()
+        folder = f"user_{user_id}"
+
+        try:
+            image_path = image_helper.save_image(data["image"], folder=folder)
+            basename = image_helper.get_basename(image_path)
+            return {"message": gettext("image_uploaded").format(basename)}, 201
+        except UploadNotAllowed:
+            extension = image_helper.get_extension(data["image"])
+            return {
+                "message": gettext("image_illigal_extension").format(extension)
+            }, 400
