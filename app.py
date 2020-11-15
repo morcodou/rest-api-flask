935e5f3ac777ffd49477a41347a512dd8fad3b91
Apple <morcodou@hotmail.com>
2020-11-14 22:20:31 -0500

    adding avatar endpoints get and put

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogo
 from resources.confirmation import Confirmation, ConfirmationByUser
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from resources.image import ImageUpload, Image
+from resources.image import ImageUpload, Image, AvatarUpload
 
 
 from ma import ma
@@ -64,6 +64,8 @@ api.add_resource(Confirmation, "/confirmation/<string:confirmation_id>")
 api.add_resource(ConfirmationByUser, "/confirmation/user/<int:user_id>")
 api.add_resource(ImageUpload, "/upload/image")
 api.add_resource(Image, "/image/<string:filename>")
+api.add_resource(AvatarUpload, "/upload/avatar")
+
 
 if __name__ == "__main__":
     db.init_app(app)
--- resources/image.py
+++ resources/image.py
@@ -26,7 +26,7 @@ class ImageUpload(Resource):
         except UploadNotAllowed:
             extension = image_helper.get_extension(data["image"])
             return {
-                "message": gettext("image_illigal_extension").format(extension)
+                "message": gettext("image_illegal_extension").format(extension)
             }, 400
 
 
@@ -37,7 +37,7 @@ class Image(Resource):
         folder = f"user_{user_id}"
 
         if not image_helper.is_filename_safe(filename):
-            return {"message": gettext("image_illigal_file_name").format(filename)}, 400
+            return {"message": gettext("image_illegal_file_name").format(filename)}, 400
 
         try:
             return send_file(image_helper.get_path(filename, folder=folder))
@@ -50,7 +50,7 @@ class Image(Resource):
         folder = f"user_{user_id}"
 
         if not image_helper.is_filename_safe(filename):
-            return {"message": gettext("image_illigal_file_name").format(filename)}, 400
+            return {"message": gettext("image_illegal_file_name").format(filename)}, 400
 
         try:
             os.remove(image_helper.get_path(filename, folder=folder))
@@ -60,3 +60,35 @@ class Image(Resource):
         except:
             traceback.print_exc()
             return {"message": gettext("image_deleted_failed")}, 500
+
+
+class AvatarUpload(Resource):
+    @jwt_required
+    def put(self):
+        data = image_schema.load(request.files)
+        user_id = get_jwt_identity()
+        filename = f"user_{user_id}"
+        folder = "avatars"
+        avatar_path = image_helper.find_image_any_format(filename, folder=folder)
+
+        if avatar_path:
+            try:
+                os.remove(avatar_path)
+            except:
+                return {
+                    "message": gettext("avatar_delete_failed").format(filename)
+                }, 500
+
+        try:
+            extension = image_helper.get_extension(data["image"].filename)
+            avatar = filename = f"{filename}{extension}"
+            avatar_path = image_helper.save_image(
+                data["image"], folder=folder, name=avatar
+            )
+            basename = image_helper.get_basename(avatar_path)
+            return {"message": gettext("avatar_uploaded").format(filename)}, 200
+        except UploadNotAllowed:
+            extension = image_helper.get_extension(data["image"])
+            return {
+                "message": gettext("avatar_illegal_extension").format(extension)
+            }, 400
