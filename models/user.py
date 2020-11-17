28cb6d4a8a421ced497143028476b95daa84a4b2
Apple <morcodou@hotmail.com>
2020-11-16 21:21:55 -0500

    add email to users table migration

--- models/user.py
+++ models/user.py
@@ -11,7 +11,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    # email = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(80), nullable=False, unique=True)
 
     # confirmation = db.relationship(
     #     "ConfirmationModel", lazy="dynamic", cascade="all, delete-orphan"
