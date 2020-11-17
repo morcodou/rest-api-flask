994ad32255bcbc76016a3daf500827cced0b3032
Apple <morcodou@hotmail.com>
2020-11-17 16:03:01 -0500

    error handle stripe

--- resources/order.py
+++ resources/order.py
@@ -2,6 +2,7 @@ from collections import Counter
 from flask_restful import Resource
 from flask import request
 from models.item import ItemModel
+import stripe
 
 from models.order import OrderModel, ItemsInOrder
 from libs.strings import gettext
@@ -33,7 +34,32 @@ class Order(Resource):
         order.save_to_db()
 
         order.set_status("failed")
-        # order.charge_with_stripe(data["token"])
-        order.set_status("complete")
 
+        try:
+            order.charge_with_stripe(data["token"])
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+            return e.code, e.http_status
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            return e.code, e.http_status
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            return e.code, e.http_status
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            return e.code, e.http_status
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            return e.code, e.http_status
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            return e.code, e.http_status
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            return {"message": gettext("order_error")}, 500
+
+        order.set_status("complete")
         return order_schema.dump(order)
