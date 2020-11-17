82005ccad0a3dc20b064fc962d966048cbf504f8
Apple <morcodou@hotmail.com>
2020-11-17 13:38:04 -0500

    add disassociated relation object

--- resources/order.py
+++ resources/order.py
@@ -1,8 +1,9 @@
+from collections import Counter
 from flask_restful import Resource
 from flask import request
 from models.item import ItemModel
 
-from models.order import OrderModel
+from models.order import OrderModel, ItemsInOrder
 from libs.strings import gettext
 
 
@@ -11,13 +12,14 @@ class Order(Resource):
     def post(cls):
         data = request.get_json()
         items = []
+        item_id_quantities = Counter(data["item_ids"])
 
-        for _id in data["item_ids"]:
+        for _id, count in item_id_quantities.most_common():
             item = ItemModel.find_by_id(_id)
             if not item:
                 return {"message": gettext("item_by_id_not_found").format(_id)}, 404
 
-            items.append(item)
+            items.append(ItemsInOrder(item_id=_id, quantity=count))
 
         order = OrderModel(items=items, status="pending")
         order.save_to_db()
